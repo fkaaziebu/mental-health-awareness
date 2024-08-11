6c707042568fdc3ff39f2fe126201521b3a002c4
fkaaziebu <frederickaziebu1998@gmail.com>
2024-08-11 18:54:02 +0100

    update team profiles v2

--- data.js
+++ data.js
@@ -1,9 +1,41 @@
 export const teams = [
-    {name: 'Michael Mohammed Issahaku', description: 'Michael Mohammed Issahaku holds a bachelor’s degree in Social Work from KNUST and is a final year MPHIL student and a graduate assistant in the department of social work at the University of Ghana. His dedication to academia and passion for social work have been defining aspects of his journey.'},
-    {name: 'Ibrahim Latifatu', description: 'Ibrahim Latifatu holds a bachelor\'s degree in English language from university of education winneba.  Latifatu currently work as a teacher.'},
-    {name: 'Queen Aboyah', description: 'Queen Aboyah is in her second year of the Master’s in Social Work Program at the University of Wisconsin Madison-Madison. She is specializing in mental health and aspires to become a psychotherapist.'},
-{name: 'Judy Aminatu Atambire', description: 'Judy Aminatu Atambire holds a Bachelor\'s degree in Social Science with a a Geography major from UDS-Wa and a Master\'s degree in Guidance and Counseling from the same University. She is currently a development practitioner in the Upper East region on a project on Adolescent reproductive health and education for both sexes'},
-{name: 'Andala Yakubu', description: 'Andala Yakubu is an Advanced Practice Social Worker (APSW). She holds a Master’s in Social Work from the University of Wisconsin-Madison. Andala practices as an outpatient psychotherapist.'},
-{name: 'Sarah Hassan', description: 'Sarah Hassan holds a Bachelor’s Degree in Social Work from KNUST, Ghana, and a Master of Social Work (MSW) from Grand Valley State University, Michigan. Sarah practices as a Medical Social Worker at Corewell Health.'},
-{name: 'Desmond Wepiah', description: 'Desmond Wepiah is behavioral science person , holds msc in organizational development,  and his area of-specialization is complete organization inquiry and system transformation, from university for development studies. He attained Bsc in human resource management. Desmond is currently the admin. Manager for Holy Family Polyclinic- Kulmasa.'}
-]
+  {
+    name: "Michael Mohammed Issahaku",
+    description:
+      "Michael Mohammed Issahaku holds a bachelor’s degree in Social Work from KNUST and is a final year MPHIL student and a graduate assistant in the department of social work at the University of Ghana. His dedication to academia and passion for social work have been defining aspects of his journey.",
+  },
+  {
+    name: "Ibrahim Latifatu",
+    description:
+      "Ibrahim Latifatu holds a bachelor's degree in English language from university of education winneba.  Latifatu currently work as a teacher.",
+  },
+  {
+    name: "Queen Aboyah",
+    description:
+      "Queen Aboyah is in her second year of the Master’s in Social Work Program at the University of Wisconsin Madison-Madison. She is specializing in mental health and aspires to become a psychotherapist.",
+  },
+  {
+    name: "Judy Aminatu Atambire",
+    description:
+      "Judy Aminatu Atambire holds a Bachelor's degree in Social Science with a a Geography major from UDS-Wa and a Master's degree in Guidance and Counseling from the same University. She is currently a development practitioner in the Upper East region on a project on Adolescent reproductive health and education for both sexes",
+  },
+  {
+    name: "Andala Yakubu",
+    description:
+      "Andala Yakubu is an Advanced Practice Social Worker (APSW). She holds a Master’s in Social Work from the University of Wisconsin-Madison. Andala practices as an outpatient psychotherapist.",
+  },
+  {
+    name: "Sarah Hassan",
+    description:
+      "Sarah Hassan holds a Bachelor’s Degree in Social Work from KNUST, Ghana, and a Master of Social Work (MSW) from Grand Valley State University, Michigan. Sarah practices as a Medical Social Worker at Corewell Health.",
+  },
+  {
+    name: "Desmond Wepiah",
+    description:
+      "Desmond Wepiah is behavioral science person , holds msc in organizational development,  and his area of-specialization is complete organization inquiry and system transformation, from university for development studies. He attained Bsc in human resource management. Desmond is currently the admin. Manager for Holy Family Polyclinic- Kulmasa.",
+  },
+  {
+    name: 'Umaira Yakubu',
+    description: 'Umaira Yakubu holds dual master\'s degrees in School Psychology and Education Specialist from Emporia State University, Emporia. She works as a school psychologist at the Gardner Edgerton School District-Kansas, USA.'
+  }
+];
